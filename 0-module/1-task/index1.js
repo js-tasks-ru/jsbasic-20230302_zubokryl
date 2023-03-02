8d153ca3ce9aef12f561af1e67647a8fc88a91b4
Svetlana Radkevich <zubokryl@gmail.com>
2023-03-02 16:29:11 +0100

    Second function

---
+++ 0-module/1-task/index1.js
@@ -0,0 +1,8 @@
+function sum(erste, zweite) {
+  let a = +prompt("Geben Sie die erste Zahl ein!");
+  let b = +prompt("Geben Sie die zweite Zahl ein!");
+  const c = a + b;
+  return c;
+}
+
+console.log(sum());
