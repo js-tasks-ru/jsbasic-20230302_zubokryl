150d815baa78677b4ab1ec7ed563c84235356863
Svetlana Radkevich <zubokryl@gmail.com>
2023-03-13 16:44:15 +0100

    Salary

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -1,3 +1,10 @@
 function sumSalary(salaries) {
-  // ваш код...
+  let sum = 0;
+	  for (let key in salaries) {
+		  if(isNaN(salaries[key]) || !isFinite(salaries[key])) {
+	    		continue;
+	    	}
+	  	sum += salaries[key];
+	 }
+  	return sum;
 }
