d75cf68b6e12a83350c1eeb251aecd0b5ff6566a
Svetlana Radkevich <zubokryl@gmail.com>
2023-03-13 14:19:02 +0100

    5task

--- 1-module/5-task/index.js
+++ 1-module/5-task/index.js
@@ -1,8 +1,6 @@
 function truncate(str, maxlength) {
-  if (str.length <= maxlength) {
-    return str;
-  } else {
-    var truncated = str.slice(0, maxlength).trim();
-    return truncated + "...";
+  if (str.length > maxlength) {
+    str = str.slice(0, maxlength - 1) + "…";
   }
+  return str;
 }
