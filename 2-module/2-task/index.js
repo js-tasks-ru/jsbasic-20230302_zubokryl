6f6dc04c7677b278edc30bf1db0ccdb19b3c7c52
Svetlana Radkevich <zubokryl@gmail.com>
2023-03-16 11:35:48 +0100

    isEmpty

--- 2-module/2-task/index.js
+++ 2-module/2-task/index.js
@@ -1,3 +1,9 @@
 function isEmpty(obj) {
-  // ваш код...
-}
+   for (const prop in obj) {
+     if (Object.prototype.hasOwnProperty.call(obj, prop)) {
+       return false;
+     }
+   }
+   return true;
+ }
+ 
